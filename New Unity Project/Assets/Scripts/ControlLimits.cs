30dab35465da93bce7b177585d64396532b12070
Arturet <arturo@pep-project.org>
2017-03-20 13:24:02 +0100

    improved the player

---
+++ New Unity Project/Assets/Scripts/ControlLimits.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlLimits : MonoBehaviour {
+
+	private Transform theTransform = null;
+	public Vector2 horizontalRange = Vector2.zero;
+
+	void Awake() {
+		theTransform = GetComponent<Transform> ();
+	}
+
+	void LateUpdate() {
+		theTransform.position = new Vector3 (
+			Mathf.Clamp (theTransform.position.x, horizontalRange.x, horizontalRange.y),
+			theTransform.position.y,
+			theTransform.position.z);
+	}
+}
