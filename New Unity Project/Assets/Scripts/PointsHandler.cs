a986c6197b711b2543985640d543768caf9b1d8f
Arturo <jimenezlopezarturo@gmail.com>
2017-03-20 21:39:45 +0100

    Current Points GUI

---
+++ New Unity Project/Assets/Scripts/PointsHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsHandler : MonoBehaviour {
+
+	public Health health;
+	private Text pointsScored;
+	private int millisecondStarted;
+	private int currentMillisecond;
+	private int currentPoints;
+	private int finalPoints;
+
+	void Awake() {
+		pointsScored = GetComponent<Text> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		millisecondStarted = System.DateTime.Now.Millisecond;
+		currentMillisecond = System.DateTime.Now.Millisecond;
+		currentPoints = 0;
+		pointsScored.text = "0";
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (health != null) {
+			string pointsToDisplay = "";
+			currentMillisecond = System.DateTime.Now.Millisecond;
+			int deltaPoints = currentMillisecond - millisecondStarted;
+			currentPoints += deltaPoints;
+			pointsToDisplay += currentPoints;
+			pointsScored.text = pointsToDisplay;
+		} else {
+			if (finalPoints != null) {
+				finalPoints = currentPoints;
+			}
+		}
+	}
+}
