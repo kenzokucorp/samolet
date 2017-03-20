4214b8a65d7f889010e338ae0db27fa0a87bbec9
Arturet <arturo@pep-project.org>
2017-03-20 10:13:25 +0100

    Basic game

---
+++ New Unity Project/Assets/Scripts/CollisionDamage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDamage : MonoBehaviour {
+
+	public float damagePoints = 100.0f;
+
+	void OnTriggerStay(Collider otherCollider) {
+		Health health = otherCollider.gameObject.GetComponent<Health> ();
+
+		if (health == null)
+			return;
+
+		health.healthPoints -= damagePoints;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ New Unity Project/Assets/Scripts/Health.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public GameObject deathParticlesPrefab = null;
+	private Transform theTransform = null;
+	public bool shouldBeDestroyOnDeath = true;
+	public bool shouldShowGameOverOnDeath = false;
+
+	public float healthPoints{
+		get {
+			return _healthPoints;
+		}
+
+		set {
+			_healthPoints = value;
+
+			if (_healthPoints <= 0) {
+				SendMessage ("Die", SendMessageOptions.DontRequireReceiver);
+
+				if (deathParticlesPrefab != null) {
+					Instantiate (deathParticlesPrefab, theTransform);
+					if(shouldBeDestroyOnDeath) {
+						Destroy (gameObject);
+					}
+				}
+
+				if (shouldShowGameOverOnDeath) {
+					//GameController.GameOver ();
+				}
+			}
+		}
+
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	[SerializeField]
+	private float _healthPoints = 100.0f;
+}
